78caa50b27b35c95e03dac6f4a9e97e694109bae
Raphael Machado <raphael.mm.91@gmail.com>
2022-10-04 04:19:22 -0300

    select working

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens:{
+      "xms": {"min": "0", "max": "425px"},
+      'sm': {'min': '640px', 'max': '767px'},
+      'md': {'min': '768px', 'max': '1023px'},
+      'lg': {'min': '1024px', 'max': '1279px'},
+      'xl': {'min': '1280px', 'max': '1535px'},
+      '2xl': {'min': '1536px'}
+    },
     extend: {
       minWidth: {
         "1": "50px",
