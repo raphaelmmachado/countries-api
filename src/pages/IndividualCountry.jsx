99f35076b3851105a488d2e007b48e9b9861550e
Raphael Machado <raphael.mm.91@gmail.com>
2022-10-04 03:30:02 -0300

    individual country page done

--- src/pages/IndividualCountry.jsx
+++ src/pages/IndividualCountry.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { Link, useParams } from "react-router-dom";
 import { GoArrowLeft } from "react-icons/go";
+import { GrMapLocation } from "react-icons/gr";
 import { useState, useEffect } from "react";
 
 function IndividualCountry() {
@@ -19,31 +20,97 @@ function IndividualCountry() {
       console.error(error);
     }
   };
-  
+
   return (
-    <main>
-      <Link to="/">
-        <div className="max-w-[80px] border-2 border-zinc-200 p-2 ml-4 rounded-lg  flex flex-row items-center">
-          <GoArrowLeft />
-          <div>Voltar</div>
-        </div>
-      </Link>
+    <main className="p-2">
+      <div
+        className="max-w-[80px] border-2
+       border-zinc-200 px-2 ml-6 rounded-lg"
+      >
+        <Link to="/">
+          <div className="flex flex-row items-center justify-center align-middle">
+            <GoArrowLeft />
+            <div>Back</div>
+          </div>
+        </Link>
+      </div>
 
       {country && country.name && country.flags && country.languages && (
-        <section className="flex flex-row items-center justify-center gap-4 min-w-full">
+        <section
+          className="flex flex-row items-center
+         justify-center gap-4 min-w-full mt-4"
+        >
           <figure id="big-flag" className="">
             <img src={country.flags.svg} alt="flag" />
           </figure>
           <div className="flex flex-col">
-            <h3 className="font-bold">{country.name.common}</h3>
-            <div className="flex flex-row">
-              <div>
+            <h3 className="text-[2.5rem] font-bold mb-4">
+              {country.name.common}
+            </h3>
+            <div className="flex flex-row gap-8">
+              <div className="leading-8">
                 <p>
-                  Oficial name: <em>{country.name.official}</em>
+                  <span className="font-bold">Oficial name:</span>{" "}
+                  {country.name.official}
                 </p>
-                <p>Languages: {Object.values(country.languages).map((key)=> `${key} | `)}</p>
+                <p>
+                  <span className="font-bold">Native name: </span>
+                  <em>
+                    {Object.values(country.name.nativeName).map(
+                      (key) => `${key.common} -`
+                    )}
+                  </em>
+                </p>
+                <p>
+                  <span className="font-bold">Languages: </span>
+                  {Object.values(country.languages).map((key) => `${key} - `)}
+                </p>
+
+                <p>
+                  <span className="font-bold">Population: </span>{" "}
+                  {country.population}
+                </p>
+                <p>
+                  <span className="font-bold">Capital: </span> {country.capital}
+                </p>
+              </div>
+              <div className="leading-8">
+                <p>
+                  <span className="font-bold">Currencies: </span>
+                  {Object.values(country.currencies).map(
+                    (key) => `${key.name} ( ${key.symbol} )`
+                  )}
+                </p>
+                <p>
+                  <span className="font-bold">Area: </span>{" "}
+                  {`${country.area}km²`}
+                </p>
+                <p>
+                  <span className="font-bold">Border:</span>{" "}
+                  {country.borders.join(" ,")}
+                </p>
+                <p>
+                  <span className="font-bold">Independent ?</span>{" "}
+                  {country.independent ? "yes" : "no"}
+                </p>
+                <p>
+                <span className="font-bold">UN Member ?</span>{" "}
+                {country.unMember ? "yes" : "no"}
+                </p>
               </div>
             </div>
+            <a href={country.maps.googleMaps} target="_blank">
+              {" "}
+              <div className="flex flex-row align-middle
+               items-center mt-2 border-2 border-zinc-200 rounded-md
+               p-2 w-fit">
+                <GrMapLocation size={24} />
+                <p className="text-lg ml-2">
+                  <span className="font-bold text-lg "> Location:</span>{" "}
+                  {country.subregion}, {country.region}
+                </p>
+              </div>
+            </a>
           </div>
         </section>
       )}
